4a0350a98ec99aba2e4593e818e2249e2c289eed
Chris-M1 <84115587+Chris-M1@users.noreply.github.com>
2024-09-17 04:06:34 +1200

    Yml Setup F

---
+++ frontend/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testEnvironment: 'jest-environment-jsdom',
+    testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  };
+  
